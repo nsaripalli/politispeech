24de2c4bc638fdcac078cc89460779d1666b56d1
nsaripalli <nithinsaripalli@gmail.com>
2020-02-15 22:24:30 -0500

    this is not working

--- gpt-2-cloud-run/app.py
+++ gpt-2-cloud-run/app.py
@@ -56,6 +56,3 @@ async def homepage(request):
     gc.collect()
     return UJSONResponse({'text': text},
                          headers=response_header)
-
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
